cfff189bac9f705ca91fcdc035fd10d290a42da6
coolkid230323 <quang0984006938@gmail.com>
2024-11-03 02:04:27 +0700

    update logoutButton

--- src/main/java/com/example/librarymanagernqc/LoginController.java
+++ src/main/java/com/example/librarymanagernqc/LoginController.java
@@ -1,14 +1,15 @@
 package com.example.librarymanagernqc;
 
+import com.example.librarymanagernqc.ManagementInterface.MainPaneController;
 import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 
-import java.util.Objects;
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -19,18 +20,36 @@ public class LoginController {
     @FXML
     private void onLoginButtonClick() {
         try {
-            // Load the new scene
-            Parent scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ManagementInterface/management-interface.fxml")));
-            Parent scene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ManagementInterface/main-pane.fxml")));
-
             // Get the current stage
             Stage stage = (Stage) loginButton.getScene().getWindow();
             stage.close();
 
+
+            // Load the new scene
+            FXMLLoader mainPaneLoader = new FXMLLoader(getClass().getResource("ManagementInterface/main-pane.fxml"));
             // Set the new scene
-            stage.setScene(new Scene(scene));
+            stage.setScene(new Scene(mainPaneLoader.load()));
             stage.show();
             stage.centerOnScreen();
+
+            MainPaneController mainPaneController = mainPaneLoader.getController();
+            mainPaneController.logoutButton.setOnMouseClicked(logoutMouseEvent -> {
+                if (logoutMouseEvent.getButton() == MouseButton.PRIMARY) {
+                    // Get the current stage
+                    Stage mainPaneStage = (Stage) mainPaneController.logoutButton.getScene().getWindow();
+                    mainPaneStage.close();
+
+                    // Set the new scene
+                    try {
+                        mainPaneStage.setScene(new Scene(new FXMLLoader(getClass().getResource("Login/login.fxml")).load()));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    mainPaneStage.show();
+                    mainPaneStage.centerOnScreen();
+                }
+            });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
